67b55debb575621ee8fca094c008757f2454dce1
Raj kumar Rabidas <152253071+RajkrRabidas@users.noreply.github.com>
2024-06-02 12:16:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".books_store", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredsliders:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".slider-box", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredsliders:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
